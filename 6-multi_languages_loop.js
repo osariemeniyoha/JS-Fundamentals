d2250fad2fd317439eb91a2911e5da35a76ad51c
osariemen iyoha <iyohapreciousosariemen@gmail.com>
2025-07-12 03:02:21 +0100

    Add script that prints 3 lines using a loop and array

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+// 6-multi_languages_loops.js
+
+const languages = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
